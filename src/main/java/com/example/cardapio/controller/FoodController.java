738c33739bb60547040dffd08e0884870387a931
Kauan Lima <kauanalmeidalima1405@gmail.com>
2024-11-12 23:31:21 -0300

    Ajustando tela de login

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("food")
+@RequestMapping("/food")
 public class FoodController {
 
     @Autowired
